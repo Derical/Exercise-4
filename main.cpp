f8024e46e919aef422598d94014d3224923c2c89
国星 张 <2296816145@qq.com>
2020-10-28 08:58:35 +0800

    练习三：高斯滤波

--- main.cpp
+++ main.cpp
@@ -22,14 +22,27 @@ int main()
 
 	// ------------------------- Exercise 2 -------------------------
 	//均值滤波
+	//VideoCapture cap(0);
+	//while (1)
+	//{
+	//	Mat src, dst;
+	//	cap >> src;
+	//	blur(src,dst,Size(kSize*2+1,kSize*2+1),Point(-1,-1));
+	//	imshow("[原图]", src);
+	//	imshow("[均值滤波后]",dst);
+	//	waitKey(30);
+	//}
+
+	// ------------------------- Exercise 3 -------------------------
+	// 高斯平滑卷积
 	VideoCapture cap(0);
+	Mat src, dst;
 	while (1)
 	{
-		Mat src, dst;
 		cap >> src;
-		blur(src,dst,Size(kSize*2+1,kSize*2+1),Point(-1,-1));
-		imshow("[原图]", src);
-		imshow("[均值滤波后]",dst);
+		GaussianBlur(src,dst,Size(kSize*2+1,kSize*2+1),0,0);
+		imshow("[原图]",src);
+		imshow("[高斯滤波后]", dst);
 		waitKey(30);
 	}
 
