3b0f17db7a4ae9061750bf3a858fd6b9ba9e0c68
国星 张 <2296816145@qq.com>
2020-10-28 09:20:41 +0800

    练习四：Sobel边缘检测

--- main.cpp
+++ main.cpp
@@ -5,6 +5,22 @@ using namespace std;
 
 int kSize = 3;
 
+void Sobel_Detect()
+{
+	VideoCapture cap(0);
+	Mat src, xdst,ydst,gray;
+	while (true)
+	{
+		cap >> src;
+		cvtColor(src,gray,COLOR_BGR2GRAY);
+		cv::Sobel(gray, xdst, CV_8UC1, 1, 0, 3);
+		cv::Sobel(gray, ydst, CV_8UC1, 0, 1, 3);
+		imshow("[x方向sobel边缘检测]", xdst);
+		imshow("[y方向sobel边缘检测]", ydst);
+		waitKey(30);
+	}
+}
+
 int main()
 {
 	// ------------------------- Exercise 1 -------------------------
@@ -35,16 +51,20 @@ int main()
 
 	// ------------------------- Exercise 3 -------------------------
 	// 高斯平滑卷积
-	VideoCapture cap(0);
-	Mat src, dst;
-	while (1)
-	{
-		cap >> src;
-		GaussianBlur(src,dst,Size(kSize*2+1,kSize*2+1),0,0);
-		imshow("[原图]",src);
-		imshow("[高斯滤波后]", dst);
-		waitKey(30);
-	}
+	//VideoCapture cap(0);
+	//Mat src, dst;
+	//while (1)
+	//{
+	//	cap >> src;
+	//	GaussianBlur(src,dst,Size(kSize*2+1,kSize*2+1),0,0);
+	//	imshow("[原图]",src);
+	//	imshow("[高斯滤波后]", dst);
+	//	waitKey(30);
+	//}
+
+	// ------------------------- Exercise 4 -------------------------
+	// Sobel边缘检测
+	Sobel_Detect();
 
 	waitKey(0);
 	return 0;
