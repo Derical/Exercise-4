d331a10a9ddd74d3bf3cbc64921a8fb0de17d460
国星 张 <2296816145@qq.com>
2020-11-11 09:33:11 +0800

    练习二

--- main.cpp
+++ main.cpp
@@ -20,17 +20,50 @@ void Exercise1()
 	cv::warpAffine(src, dst, rot, bbox.size());
 	imshow("[原图]", src);
 	imshow("[变换后]", dst);
-
+	waitKey(0);
 }
+// ------------------------- Exercise 2 -------------------------
+// 霍夫变换
+void Exercise2()
+{
+	Mat src = imread("C:\\Users\\STAR ZHANG\\Pictures\\18.jpg");
+	imshow("[原图]", src);
+	Mat mid;
+	Mat dst;
+	Mat gray;
+	cvtColor(src,gray, COLOR_BGR2GRAY);
+	Canny(gray,mid,50,200,3);
+	// 进行边缘检测
+	// ----- 选择用line承载直线参数 -----
+	std::vector<cv::Vec2f> lines;
+	HoughLines(mid,lines,1,CV_PI/180,100);
+	//绘制直线
+	std::vector<cv::Vec2f>::iterator it = lines.begin();
+	for (; it != lines.end(); ++it)
+	{
+		float rho = (*it)[0], theta = (*it)[1];
+		cv::Point pt1, pt2;
+		double a = cos(theta);
+		double b = sin(theta);
+		double x0 = a * rho;
+		double y0 = b * rho;
+		pt1.x = cv::saturate_cast<int>(x0 + 1000 * (-b));
+		pt1.y = cv::saturate_cast<int>(y0 + 1000 * (a));
+		pt2.x = cv::saturate_cast<int>(x0 - 1000 * (-b));
+		pt2.y = cv::saturate_cast<int>(y0 - 1000 * (a));
+		cv::line(src, pt1, pt2, cv::Scalar(0, 0, 255), 1);
+	}
 
-
-
+	imshow("[绘制直线后]", src);
+	waitKey();
+}
 int main()
 {
 	// ------------------------- Exercise 1 -------------------------
-	Exercise1();
+	//Exercise1();
 
-
+	// ------------------------- Exercise 2 -------------------------
+	Exercise2();
 
 	waitKey(0);
 	return 0;
