85556e84e752a0c97376c1eae634c15650810ab1
国星 张 <2296816145@qq.com>
2020-11-18 13:18:00 +0800

    练习三

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 using namespace cv;
 using namespace std;
 
+// ------------------------- Exercise1 -------------------------
 void Exercise1( )
 {
 	cv::Mat src = imread("C:\\Users\\STAR ZHANG\\Pictures\\cv81.png");
@@ -54,6 +55,7 @@ void Exercise1( )
 	imshow("[效果图]", disMat);
 	waitKey(0);
 }
+// ------------------------- Exercise2 -------------------------
 void Exercise2()
 {
 	cv::Mat src = imread("C:\\Users\\STAR ZHANG\\Pictures\\cv82.jpg");
@@ -102,12 +104,101 @@ void Exercise2()
 	imshow("[效果图]",disMat);
 	waitKey(0);
 }
+// ------------------------- Exercise3 -------------------------
+
+void Exercise3()
+{
+	//设置可调整图片大小的窗口来显示原图
+	cv::Mat src = imread("C:\\Users\\STAR ZHANG\\Pictures\\cv83.jpg");
+	//namedWindow("[原图]",WINDOW_NORMAL);
+	//imshow("[原图]",src);
+	cv::Mat disMat;
+	src.copyTo(disMat);
+	
+	/* ---------------------------- 分离RGB三通道 -----------------------------
+	*		在Image Watch中观察发现效果并不好
+	*		换用HSV空间
+	*/
+	//定义vector作为三个通道的容器
+	//std::vector<cv::Mat> channels;
+	//cv::split(src, channels);
+	//cv::Mat Blue = channels.at(0);
+	//cv::Mat Green = channels.at(1);
+	//cv::Mat Red = channels.at(2);
+
+	// ------------------------------------------------------------------------
+
+	// 转换为HSV
+	int iLowH = 156;			
+	int iHighH = 180;
+	int iLowS = 43;
+	int iHighS = 255;
+	int iLowV = 46;
+	int iHighV = 255;
+	cv::Mat hsvMat;
+	vector<Mat> hsvSplit;
+	cvtColor(src,hsvMat,COLOR_BGR2HSV);
+	split(hsvMat, hsvSplit);
+
+	// 直方图均值化，增强图像亮度及对比度
+	equalizeHist(hsvSplit[2], hsvSplit[2]);
+	merge(hsvSplit, hsvMat);
+	
+	// 二值化
+	Mat thresholdedMat;
+	inRange(hsvMat, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), thresholdedMat);
+
+	// 闭运算
+	Mat tmp;
+	Mat kernel = getStructuringElement(MORPH_ELLIPSE,Size(5,5));
+	morphologyEx(thresholdedMat,tmp,MORPH_CLOSE,kernel,Point(-1,-1),16);
+	//namedWindow("tmp",WINDOW_NORMAL);
+	//imshow("tmp", tmp);
+
+
+	// 寻找轮廓
+	std::vector<vector<Point>> contours;
+	std::vector<Vec4i> hierarchy;
+	findContours(tmp, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_NONE);
+	cout << "轮廓的个数为 :" << contours.size() << std::endl;
+
+	// 获得最小外接四边形
+	for (int i = 0; i < contours.size(); ++i)
+	{
+		RotatedRect rbox = minAreaRect(contours[i]);
+		int height = rbox.size.height;
+		int width = rbox.size.width;
+
+		//限制宽长比
+		if (height != 0 && width != 0)
+		{
+			float wdrat = (float)width / height;
+			if (wdrat >= 0.8 && wdrat <= 1.2)
+			{
+				cout << "The width is " << rbox.size.width << endl;
+				cout << "The height is " << rbox.size.height << endl;
+				cout << "The width divided by the height is " << wdrat << endl;
+
+				//绘制轮廓
+				cv::Point2f vtx[4];
+				rbox.points(vtx);
+				for (int i = 0; i < 4; ++i)
+					cv::line(disMat, vtx[i], vtx[i < 3 ? i + 1 : 0], cv::Scalar(0, 255, 255), 4);
+			}
+		}
+	}
+	namedWindow("[效果图]", WINDOW_GUI_NORMAL);
+	imshow("[效果图]",disMat);
+	waitKey(0);
+}
 
 int main()
 {
 	// 练习一
 	//Exercise1();
-	//练习二
-	Exercise2();
+	// 练习二
+	//Exercise2();
+	// 练习三
+	Exercise3();
 	waitKey(0);
 }
